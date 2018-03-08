fa8073cfec0cbd96aed9d4ee83aa00d2383287b7
5pecia1 <pdpxpd@gmail.com>
2018-03-08 16:42:28 +0000

    CR(2) and D

--- note/config/routes.rb
+++ note/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'home/new'
 
   post 'home/create'
+  
+  get 'home/destroy/:post_id' => 'home#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
